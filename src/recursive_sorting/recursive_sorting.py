a8f3d8be1aa668ee8c535a965712ed721e0db9d6
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-14 13:06:51 -0400

    add section heading comments

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -19,6 +19,10 @@ sys.path.append("../")
 
 from compare import compare_ascending
 
+########################################
+#   QUICK SORT
+########################################
+
 
 def quick_sort(array, compare=compare_ascending):
     # TODO: guided
@@ -31,7 +35,9 @@ def merge(arrayA, arrayB, compare=compare_ascending):
     merged_array = [0] * elements
     # TODO: mvp
 
-    return merged_array
+########################################
+#   MERGE SORT
+########################################
 
 
 def merge_sort(array, compare=compare_ascending):
@@ -40,7 +46,12 @@ def merge_sort(array, compare=compare_ascending):
     return array
 
 
-# STRETCH: implement an in-place merge sort algorithm
+########################################
+#   MERGE SORT - IN PLACE
+########################################
+#   STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(array, start, mid, end, compare=compare_ascending):
     # TODO: stretch
 
@@ -53,8 +64,13 @@ def merge_sort_in_place(array, l, r, compare=compare_ascending):
     return array
 
 
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+########################################
+#   TIM SORT
+########################################
+#   STRETCH: implement the Timsort function below
+#   hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+
+
 def tim_sort(array, compare=compare_ascending):
     # TODO: stretch
 
